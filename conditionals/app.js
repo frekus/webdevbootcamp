7b8db9fcc59d4105332f48e625716c511cda29f2
udomk <kufrekus4@gmail.com>
2022-06-11 22:06:41 +0100

    webdevbootcamp

--- conditionals/app.js
+++ conditionals/app.js
@@ -7,9 +7,22 @@
 //     console.log("YOUR NUMBER IS GREATER THAN 0.5!!!")
 //     console.log(random);
 // }
-const dayOfWeek = 'Saturday';
-if (dayOfWeek === 'Monday'){
+
+const dayOfWeek = prompt('Enter a day').toLowerCase();
+if (dayOfWeek === 'monday'){
     console.log("UGHH I HATE MONDAYS!")
-} else if (dayOfWeek === 'Saturday'){
+} else if (dayOfWeek === 'saturday'){
     console.log("YAY I LOVE SATURDAYS")
+    }
+else if (dayOfWeek === 'friday'){
+    console.log("FRIDAY ARE DECENT ESPECIALLY AFTER WORK!")
 }
+else{
+    console.log("MEH")
+}
+// const age = 2;
+// if (age < 5){
+//     console.log("You are a baby. You get in for free")
+// } else if(age < 10){
+//     console.log("You are a child. You pay $10")
+// }
