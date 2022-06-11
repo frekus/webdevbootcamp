ea5b865d229640cf6d44da4d587662390994ca8f
udomk <kufrekus4@gmail.com>
2022-06-11 21:45:40 +0100

    webdevbootcamp

--- conditionals/app.js
+++ conditionals/app.js
@@ -1,9 +1,15 @@
-let random = Math.random();
-if(random < 0.5) {
-    console.log("YOUR NUMBER IS LESS THAN 0.5!!")
-    console.log(random);
-}
-if(random >=0.5){
-    console.log("YOUR NUMBER IS GREATER THAN 0.5!!!")
-    console.log(random);
+// let random = Math.random();
+// if(random < 0.5) {
+//     console.log("YOUR NUMBER IS LESS THAN 0.5!!")
+//     console.log(random);
+// }
+// if(random >=0.5){
+//     console.log("YOUR NUMBER IS GREATER THAN 0.5!!!")
+//     console.log(random);
+// }
+const dayOfWeek = 'Saturday';
+if (dayOfWeek === 'Monday'){
+    console.log("UGHH I HATE MONDAYS!")
+} else if (dayOfWeek === 'Saturday'){
+    console.log("YAY I LOVE SATURDAYS")
 }
