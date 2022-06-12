6ec19cb5cf68dda738ce02147d7214437a11f65c
udomk <kufrekus4@gmail.com>
2022-06-12 20:39:08 +0100

    webdevbootcamp

---
+++ logical_operators/app.js
@@ -0,0 +1,39 @@
+// const password = prompt("Enter your password");
+// if(password.length >= 6 && password.indexOf(' ') === -1){
+//     console.log("VALID PASSWORD!")
+// }else {
+//     console.log("INCORRECT FORMAT FOR PASSWORD!")
+// }
+
+// const age = 7;
+// if (age >= 0 && age < 5 || age >= 65){
+//     console.log("FREE");
+// }
+//     else if (age >= 5 && age < 10){
+//         console.log("$10")
+//     }
+//     else if (age >= 10 && age < 65) {
+//         console.log("$20")
+//     }
+//     else{
+//         console.log("INVALID AGE")
+//     }
+    
+    // const age = 20;
+    // if (!(age >= 0 && age < 5 || age >= 65)){
+    //     console.log("YOU ARE NOT A BABY OR A SENIOR");
+    // }
+const day = 2;
+switch (day){
+    case 1:
+        console.log("MONDAY");
+        break;
+    case 2:
+        console.log("TUESDAY");
+        break;
+    case 3:
+        console.log("WEDNESDAY");
+        break;
+    default:
+        console.log("I DON'T KNOW THAT")
+}
