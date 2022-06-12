d3b78988507c4e87c6e86a4ea597a3daa2de259d
udomk <kufrekus4@gmail.com>
2022-06-12 19:35:48 +0100

    webdevbootcamp

--- conditionals/app.js
+++ conditionals/app.js
@@ -8,21 +8,33 @@
 //     console.log(random);
 // }
 
-const dayOfWeek = prompt('Enter a day').toLowerCase();
-if (dayOfWeek === 'monday'){
-    console.log("UGHH I HATE MONDAYS!")
-} else if (dayOfWeek === 'saturday'){
-    console.log("YAY I LOVE SATURDAYS")
-    }
-else if (dayOfWeek === 'friday'){
-    console.log("FRIDAY ARE DECENT ESPECIALLY AFTER WORK!")
-}
-else{
-    console.log("MEH")
-}
+// const dayOfWeek = prompt('Enter a day').toLowerCase();
+// if (dayOfWeek === 'monday'){
+//     console.log("UGHH I HATE MONDAYS!")
+// } else if (dayOfWeek === 'saturday'){
+//     console.log("YAY I LOVE SATURDAYS")
+//     }
+// else if (dayOfWeek === 'friday'){
+//     console.log("FRIDAY ARE DECENT ESPECIALLY AFTER WORK!")
+// }
+// else{
+//     console.log("MEH")
+// }
 // const age = 2;
 // if (age < 5){
 //     console.log("You are a baby. You get in for free")
 // } else if(age < 10){
 //     console.log("You are a child. You pay $10")
 // }
+
+const password = prompt("Please enter a new password");
+if(password.length >= 6){
+    if (password.indexOf(' ') === -1){
+        console.log("Valid Password!")
+    }else {
+        console.log("Password cannot contain spaces!")
+    }
+    
+}else{
+    console.log("PASSWORD TOO SHORT. MUST BE 6 PLUS XTERS")
+}
