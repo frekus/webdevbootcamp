6e587b1644df67ddea88ff63a19542f6a1ba0e67
udomk <kufrekus4@gmail.com>
2022-06-29 02:28:23 +0100

    webdevbootcamp

--- Basicfunctions/app.js
+++ Basicfunctions/app.js
@@ -1,7 +1,38 @@
-function singSong(){
-    console.log('Doh');
-    console.log('Reh');
-    console.log('Mi');
-}
+// function singSong(){
+//     console.log('Doh');
+//     console.log('Reh');
+//     console.log('Mi');
+// }
 
-singSong()
+// singSong()
+
+// function greet(firstName, lastName){
+//     console.log(`Hey there, ${firstName} ${lastName,[0]}.`)
+// }
+
+// function repeat(str, numTimes){
+//     let result = '';
+//     for (let i = 0; i < numTimes; i++){
+//         result += str;
+//     }
+//     console.log(result);
+
+// }
+
+// function rant(message){
+//     console.log(message.toUpperCase());
+//     console.log(message.toUpperCase());
+//     console.log(message.toUpperCase());
+// }
+
+// rant('woop')
+
+function isSnakeEyes(num1, num2){
+    if(num1 === 1 && num2 === 1){
+        console.log('Snake Eyes!');
+    }else {
+        console.log('Not Snake Eyes');
+    }
+}
+isSnakeEyes(1,1)
+//isSnakeEyes(4,2)
