8c81a57273609a5165af8f6df6188ff31bd2a7e9
udomk <kufrekus4@gmail.com>
2022-07-12 11:09:36 +0100

    webdevbootcamp

--- Basicfunctions/app.js
+++ Basicfunctions/app.js
@@ -85,7 +85,7 @@ function capitalize(word){
 
 console.log(capitalize(word)); */
 
-const arr = [];
+/* const arr = [];
 let total = 0;
 function sumArray(arr){
     for(let i = 0; i < arr.length; i++){
@@ -94,5 +94,21 @@ function sumArray(arr){
     return total;
     //console.log(total)
 }
-console.log(sumArray([100,10, -3]));
+console.log(sumArray([100,10, -3])); */
+
+
+ function returnDay(day){
+    let dayOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    if(day < 1 || day > 7){
+        return null;
+    }else{
+        return dayOfWeek[day - 1];
+    }
+}
+
+console.log(returnDay(7))
+
+
+
+
 
