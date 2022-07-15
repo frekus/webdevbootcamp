b52157a38f541074ce3bd420c831dd58d75107df
udomk <kufrekus4@gmail.com>
2022-07-15 03:42:35 +0100

    wdb

--- Basicfunctions/app.js
+++ Basicfunctions/app.js
@@ -152,5 +152,90 @@ numbers.forEach(print) */
     console.log(el);
 } */
 
+/* const doubles = numbers.map(function(num){
+    return num * 2;
+})
+ */
+ const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 93
+    }
+]
+/*
+const titles = movies.map(function (movie){
+    return movie.title.toUpperCase();
+}) */
 
+// DO NOT ALTER THE FOLLOWING CODE:
+/* const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
 
+// Write your code here
+const firstNames = fullNames.map(function (name){
+    return name.first;
+}) */
+
+/* const add = function(x, y) {
+    return x + y;
+} */
+
+/* const add = (x, y) => {
+    return x + y;
+} */
+
+/* const square = num => {
+    return num * num;
+} */
+
+/* const rollDie = () => {
+    return Math.floor(Math.random() * 6) + 3
+} */
+
+/* const greet = name => {
+    return `Hey ${name}!`;
+} */
+
+/* const rollDie = () => (
+     Math.floor(Math.random() * 6) + 1
+) */
+
+// const add = (x, y) => x + y
+
+/* const newMovies = movies.map(function (movie) {
+    return `${movie.title} - ${movie.score / 10}`
+}) */
+
+/* const newMovies = movies.map(movie =>
+    `${movie.title} - ${movie.score / 10}`
+) */
+
+ 
+/* console.log("Hello")
+setTimeout( () => {
+    console.log("Goodbye")
+
+}, 3000) 
+
+setTimeout( () => {
+    console.log("Are you still there?")
+
+}, 30000) */
+
+// TO USE CLEARINTERVAL ASSIGN
+/* const id = setInterval(() => {
+    console.log(Math.random())
+}, 2000); */
+
+// clearInterval(id)
